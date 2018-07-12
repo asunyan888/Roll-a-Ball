7d1f004741de1930cc9015f5a6c496bc90701b40
川崎あすか <asuka.k@kawasakiasukanoMac-mini.local>
2018-07-12 13:42:18 +0900

    Roll-a-Ball

---
+++ Roll-a-Ball/Assets/Script/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider hit)
+	{
+        // 接触対象がPlayerであれば
+        if (hit.CompareTag ("Player"))
+        {
+            Destroy(gameObject);
+        }
+		
+	}
+}
