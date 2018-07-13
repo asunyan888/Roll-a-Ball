23d01d4b751e66627b0597e8d7c8cccd8919eec7
Asuka Kawasaki <Asuka@Asuka888.local>
2018-07-13 16:14:24 +0900

    Endボタンに機能追加

---
+++ Roll-a-Ball/Assets/Script/GameSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameSystem : MonoBehaviour {
+
+    public void GameStart(){
+        SceneManager.LoadScene("Stage01");
+    }
+
+    public void GameEnd(){
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        #elif UNITY_WEBPLAYER
+         Application.OpenURL("http://www.yahoo.co.jp/");
+    #else
+        Application.Quit();
+    #endif 
+    }
+}
